2e150ea7d49edfeb2388bbfb2530b748eeb74456
Marija Dragojevic <marija.dragojevic88@gmail.com>
2016-06-07 13:28:49 +0200

    Algorithm interfaces added.

---
+++ Machine_learning/Algorithm.h
@@ -0,0 +1,50 @@
+/*
+ * Algorithm.h
+ *
+ *  Created on: Jun 7, 2016
+ *      Author: marija
+ */
+
+#ifndef ALGORITHM_H_
+#define ALGORITHM_H_
+
+#include <map>
+#include <string>
+#include <memory>
+
+class Data_set;
+class Data;
+
+class Algorithm_parameters {
+public:
+	typedef std::map<std::string, std::string> param_map;
+	Algorithm_parameters(const param_map& params = {}) : parameters(params) {};
+
+	std::string get(const std::string& name);
+
+	int get_int(const std::string& name);
+
+	double get_double(const std::string& name);
+
+	std::string safe_get(const std::string& name, const std::string& backup) const;
+
+private:
+	param_map parameters;
+};
+
+template<class T>
+class Classifier {
+public:
+	virtual ~Classifier(){};
+    virtual void classify(const Data & d, T & out) = 0;
+};
+
+template<class T>
+class Algorithm {
+public:
+	virtual ~Algorithm(){};
+	virtual void setup(const Algorithm_parameters& parameters) = 0;
+	virtual std::shared_ptr<Classifier<T>> learn(const Data_set & data_set) = 0;
+};
+
+#endif /* ALGORITHM_H_ */
--- Machine_learning/Cross_validation.h
+++ Machine_learning/Cross_validation.h
@@ -6,74 +6,33 @@
 #include <iostream>
 #include <algorithm>
 #include "Dataset.h"
+#include "Algorithm.h"
 
-class Algorithm_parameters {
+class Spoof_classifier:public Classifier<double> {
 public:
-	typedef std::map<std::string, std::string> param_map;
-	Algorithm_parameters(const param_map& params = {}) : parameters(params) {};
-
-	std::string get(const std::string& name) {
-		return safe_get(name, "");
-	}
-
-	int get_int(const std::string& name) {
-		return std::stoi(safe_get(name, "0"));
-	}
-
-	double get_double(const std::string& name) {
-		return std::stod(safe_get(name, "0.0"));
-	}
-
-	std::string safe_get(const std::string& name, const std::string& backup) const {
-		auto found_it = parameters.find(name);
-		if (found_it == parameters.end()) {
-			return backup;
-		}
-		else {
-			return found_it->second;
-		}
-	}
-
+	Spoof_classifier(double f): error{f}{}
+    void classify(const Data & d, double & out) {
+		int seed = (int)std::chrono::system_clock::now().time_since_epoch().count();
+		out = std::min( std::max( error + ((seed % 100)-50) / 1000.0, 0.0 ), 1.0);
+    }
 private:
-	param_map parameters;
+	double error;
 };
 
-class Learning_algorithm {
-public:
-	//virtual ~Learning_algorithm() = 0;
-	virtual void setup(const Algorithm_parameters& parameters) = 0;
-	virtual void reload() = 0;
-	virtual void learn(const Data_set & data_set) = 0;
-	virtual double classify(const Data& data) = 0;
-};
-
-class Spoof_la : public Learning_algorithm {
+class Spoof_la : public Algorithm<double> {
 private:
 	Algorithm_parameters parameters;
-	double error;
 
 public:
-	Spoof_la() {
+	Spoof_la():Algorithm{} {
 	}
 
 	virtual void setup(const Algorithm_parameters& param) {
 		parameters = param;
 	}
 
-	virtual void reload() {
-		//unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-		//error = 0.5 + (seed % 1000) / 2000.0;
-
-		error = parameters.get_double("error");
-	}
-
-	virtual void learn(const Data_set & data_set) {
-		// Spoof
-	}
-
-	virtual double classify(const Data& data) {
-		int seed = (int)std::chrono::system_clock::now().time_since_epoch().count();
-		return std::min( std::max( error + ((seed % 100)-50) / 1000.0, 0.0 ), 1.0);
+	virtual std::shared_ptr<Classifier<double>> learn(const Data_set & data_set) {
+		return std::make_shared<Spoof_classifier>(parameters.get_double("error"));
 	}
 };
 
@@ -88,17 +47,17 @@ public:
 		return k_fold(data_set, algorithm, k);
 	}
 
-	static std::vector<double> k_fold(const Data_set& data_set, Learning_algorithm& algorithm, int k) {
+	static std::vector<double> k_fold(const Data_set& data_set, Algorithm<double>& algorithm, int k) {
 		k = k < 2 ? 2 : (k > data_set.get_size() ? data_set.get_size() : k);
 		Data_set train, validate;
 		std::vector<double> errors(k);
 		for (int fold = 0; fold < k; fold++) {
 			data_set.distribute_fold(train, validate, k, fold);
-			algorithm.reload();
-			algorithm.learn(train);
+			auto classifier = algorithm.learn(train);
 			double avg_error = 0.0;
 			for (int val_idx = 0; val_idx < validate.get_size(); val_idx++) {
-				double error = algorithm.classify( validate.get_elem(val_idx) );
+				double error;
+				classifier->classify( validate.get_elem(val_idx), error);
 				avg_error += error;
 			}
 			avg_error /= validate.get_size();
@@ -137,4 +96,3 @@ public:
 		}*/
 	}
 };
-
